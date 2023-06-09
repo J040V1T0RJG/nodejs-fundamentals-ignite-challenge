5ad912d39733f2bf8cfed2fc64e52e5a0304bbb8
João Vitor <joaovitor.jovg73@gmail.com>
2023-06-09 17:22:48 -0300

    :sparkles: feat: api routes POST('/tasks') and GET('/tasks')

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database/index.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+      return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const {title, description} = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: null
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  }
+]
