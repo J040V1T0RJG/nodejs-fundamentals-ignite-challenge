0c30a46a8e1e519b306f6a0c164ff3d0282c7d2a
João Vitor <joaovitor.jovg73@gmail.com>
2023-06-10 14:40:04 -0300

    :sparkles: feat: api route DELETE('/tasks/:id')

--- src/routes.js
+++ src/routes.js
@@ -67,5 +67,22 @@ export const routes = [
 
       return res.writeHead(204).end()
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const isIdValid = database.find('tasks', id)
+
+      if(!isIdValid) {
+        return res.writeHead(401).end()
+      }
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    }
   }
 ]
